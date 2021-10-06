c04200c6f3771bb2405b55d20ff6b9d2aa14058c
Jen Caza <jencaza@gmail.com>
2021-10-06 16:46:32 -0400

    add banana bread

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (const ingredients of ingredients) {
+  console.log(ingredients)
+}
+
+let i = 8;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i--;
+}
